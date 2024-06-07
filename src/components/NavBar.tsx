aa5f15cdb3199649a2dda6e917c1117d1b034cd9
unknown <23esingh@gmail.com>
2024-06-07 14:28:48 -0400

    darkmode added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import { AspectRatio, HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../images/logo1.webp";
+import logo from "../images/logo2.webp";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image src={logo} alt="Eshan" />
+      <Image src={logo} alt="Eshan" boxSize={"100px"} />
       <Text>NavBar</Text>
     </HStack>
   );
