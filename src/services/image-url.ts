11ee33f3de42c44552c2c244ee091d542bf23ca1
unknown <23esingh@gmail.com>
2024-06-12 12:10:26 -0400

    handle no-image case

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeholder from "../images/image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeholder;
   const targ = "media/";
   const index = url.indexOf(targ) + targ.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
